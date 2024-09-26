f1597fd9029326a6f4dfc032e5a6e72600ae3da5
trindadedev13 <devsuay@gmail.com>
2024-09-26 18:39:59 -0300

    build: refactor gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.agp.lib)
+}
+
+android {
+    namespace = "com.sparkleseditor.android"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.sparkleseditor.android"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.navigation.ui)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.appcompat)
+    implementation(libs.navigation.fragment)
+    implementation(libs.constraintlayout)
+    implementation(libs.material)
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.activity)
+    implementation(libs.sora.editor.bom)
+    implementation(libs.sora.editor)
+}
