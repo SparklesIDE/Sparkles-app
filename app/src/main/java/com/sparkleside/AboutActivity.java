a5b3de29e392a0ff59d56477364df4f03fe63e6f
trindadedev13 <devsuay@gmail.com>
2024-10-22 21:24:35 -0300

    fix: dont use new

--- app/src/main/java/com/sparkleside/AboutActivity.java
+++ app/src/main/java/com/sparkleside/AboutActivity.java
@@ -139,7 +139,7 @@ public class AboutActivity extends AppCompatActivity {
     }
     
     private void peekAndPop(String name, String imageUrl, View v) {
-        AboutPreviewBinding peekBinding = new AboutPreviewBinding.inflate(getLayoutInflater());
+        var peekBinding = AboutPreviewBinding.inflate(getLayoutInflater());
         peekBinding.title.setText(name);
         Glide
             .with(this)
