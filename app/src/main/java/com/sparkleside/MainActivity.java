414dff967d99bfc3bd3e14e8732ceed499c50eb4
trindadedev13 <devsuay@gmail.com>
2024-10-22 21:51:25 -0300

    feat: Format code & Cleanups
    @yamenher stop set the toolbar text size bro, in some phones this is bad

--- app/src/main/java/com/sparkleside/MainActivity.java
+++ app/src/main/java/com/sparkleside/MainActivity.java
@@ -1,154 +1,143 @@
 package com.sparkleside;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Build;
-import androidx.activity.EdgeToEdge;
-import android.content.res.Configuration;
-import android.content.SharedPreferences;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.MarginLayoutParamsCompat;
-import android.view.ViewGroup.MarginLayoutParams;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.sparkleside.databinding.ActivityMainBinding;
+
 import com.sparkleside.component.ExpandableLayout;
+import com.sparkleside.databinding.ActivityMainBinding;
 import io.github.rosemoe.sora.widget.schemes.EditorColorScheme;
-// import com.sparkleside.component.terminal.Terminal;
-import androidx.core.content.ContextCompat;
-import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
-  private ActivityMainBinding binding;
-  private Intent intent;
-    
+    private ActivityMainBinding binding;
+    private Intent intent;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    EdgeToEdge.enable(this);
-    super.onCreate(savedInstanceState);
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
-    setContentView(binding.getRoot());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    setSupportActionBar(binding.toolbar);
-    binding.toolbox.setExpansion(false);
-    binding.toolbox.setDuration(200);
-    binding.toolbox.setOrientatin(ExpandableLayout.VERTICAL);
-    if (Build.VERSION.SDK_INT >= 26) {
-        binding.term.setTooltipText(getString(R.string.tooltip_terminal));
-        binding.search.setTooltipText(getString(R.string.tooltip_search));
-        binding.file.setTooltipText(getString(R.string.tooltip_new_file));   
-        binding.settings.setTooltipText(getString(R.string.tooltip_settings));
-    }        
-    binding.fab.setOnClickListener(
-        v -> Toast.makeText(MainActivity.this, "ComingSoon", Toast.LENGTH_SHORT).show());
-    binding.settings.setOnClickListener(
-        v -> {
-          Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-          startActivity(intent);
-        });
-    binding.editor.setTypefaceText(
-        Typeface.createFromAsset(getAssets(), "fonts/jetbrainsmono" + ".ttf"));
-    int currentNightMode =
-        getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-    if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+        setSupportActionBar(binding.toolbar);
+        binding.toolbox.setExpansion(false);
+        binding.toolbox.setDuration(200);
+        binding.toolbox.setOrientatin(ExpandableLayout.VERTICAL);
 
-      var scheme = binding.editor.getColorScheme();
-      scheme.setColor(
-          EditorColorScheme.WHOLE_BACKGROUND,
-          ContextCompat.getColor(this, R.color.md_theme_surface));
-      scheme.setColor(
-          EditorColorScheme.CURRENT_LINE,
-          ContextCompat.getColor(this, R.color.md_theme_surfaceBright));
-      scheme.setColor(
-          EditorColorScheme.LINE_NUMBER_PANEL,
-          ContextCompat.getColor(this, R.color.md_theme_surface));
-      scheme.setColor(
-          EditorColorScheme.LINE_NUMBER_BACKGROUND,
-          ContextCompat.getColor(this, R.color.md_theme_surface));
-      scheme.setColor(EditorColorScheme.KEYWORD, 0xFF42BE6A);
-      scheme.setColor(EditorColorScheme.FUNCTION_NAME, 0xFF62DE8A);
-      scheme.setColor(EditorColorScheme.TEXT_NORMAL, 0XFFA2D2A9);
-      scheme.setColor(EditorColorScheme.OPERATOR, 0xFFDDE5DB);
+        if (Build.VERSION.SDK_INT >= 26) {
+            binding.term.setTooltipText(getString(R.string.tooltip_terminal));
+            binding.search.setTooltipText(getString(R.string.tooltip_search));
+            binding.file.setTooltipText(getString(R.string.tooltip_new_file));
+            binding.settings.setTooltipText(getString(R.string.tooltip_settings));
+        }
 
-    } else {
+        binding.fab.setOnClickListener(v -> 
+            Toast.makeText(MainActivity.this, "ComingSoon", Toast.LENGTH_SHORT).show()
+        );
 
-      var scheme = binding.editor.getColorScheme();
-      scheme.setColor(
-          EditorColorScheme.WHOLE_BACKGROUND,
-          ContextCompat.getColor(this, R.color.md_theme_surface));
-      scheme.setColor(
-          EditorColorScheme.CURRENT_LINE,
-          ContextCompat.getColor(this, R.color.md_theme_surfaceDim));
-      scheme.setColor(
-          EditorColorScheme.LINE_NUMBER_PANEL,
-          ContextCompat.getColor(this, R.color.md_theme_surface));
-      scheme.setColor(
-          EditorColorScheme.LINE_NUMBER_BACKGROUND,
-          ContextCompat.getColor(this, R.color.md_theme_surface));
-      scheme.setColor(EditorColorScheme.KEYWORD, 0xFF42BE6A);
-      scheme.setColor(EditorColorScheme.FUNCTION_NAME, 0xFF62DE8A);
-      scheme.setColor(EditorColorScheme.TEXT_NORMAL, 0XFF629269);
-      scheme.setColor(EditorColorScheme.OPERATOR, 0xFFDDE5DB);
-    }
-
-    ViewCompat.setOnApplyWindowInsetsListener(
-        binding.fab,
-        (v, windowInsets) -> {
-          Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
-          MarginLayoutParams mlp = (MarginLayoutParams) v.getLayoutParams();
-          // mlp.leftMargin = insets.left;
-          mlp.bottomMargin = insets.bottom;
-          //  mlp.rightMargin = insets.right;
-          v.setLayoutParams(mlp);
-          return WindowInsetsCompat.CONSUMED;
+        binding.settings.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
         });
 
-    binding.term.setOnClickListener(
-        it -> startActivity(new Intent(getApplicationContext(), TermuxActivityCompat.class)));
-  }
+        binding.editor.setTypefaceText(
+            Typeface.createFromAsset(getAssets(), "fonts/jetbrainsmono.ttf")
+        );
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-    return true;
-  }
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        var scheme = binding.editor.getColorScheme();
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    int id = item.getItemId();
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            scheme.setColor(EditorColorScheme.WHOLE_BACKGROUND,
+                ContextCompat.getColor(this, R.color.md_theme_surface));
+            scheme.setColor(EditorColorScheme.CURRENT_LINE,
+                ContextCompat.getColor(this, R.color.md_theme_surfaceBright));
+            scheme.setColor(EditorColorScheme.LINE_NUMBER_PANEL,
+                ContextCompat.getColor(this, R.color.md_theme_surface));
+            scheme.setColor(EditorColorScheme.LINE_NUMBER_BACKGROUND,
+                ContextCompat.getColor(this, R.color.md_theme_surface));
+            scheme.setColor(EditorColorScheme.KEYWORD, 0xFF42BE6A);
+            scheme.setColor(EditorColorScheme.FUNCTION_NAME, 0xFF62DE8A);
+            scheme.setColor(EditorColorScheme.TEXT_NORMAL, 0xFFA2D2A9);
+            scheme.setColor(EditorColorScheme.OPERATOR, 0xFFDDE5DB);
+        } else {
+            scheme.setColor(EditorColorScheme.WHOLE_BACKGROUND,
+                ContextCompat.getColor(this, R.color.md_theme_surface));
+            scheme.setColor(EditorColorScheme.CURRENT_LINE,
+                ContextCompat.getColor(this, R.color.md_theme_surfaceDim));
+            scheme.setColor(EditorColorScheme.LINE_NUMBER_PANEL,
+                ContextCompat.getColor(this, R.color.md_theme_surface));
+            scheme.setColor(EditorColorScheme.LINE_NUMBER_BACKGROUND,
+                ContextCompat.getColor(this, R.color.md_theme_surface));
+            scheme.setColor(EditorColorScheme.KEYWORD, 0xFF42BE6A);
+            scheme.setColor(EditorColorScheme.FUNCTION_NAME, 0xFF62DE8A);
+            scheme.setColor(EditorColorScheme.TEXT_NORMAL, 0xFF629269);
+            scheme.setColor(EditorColorScheme.OPERATOR, 0xFFDDE5DB);
+        }
 
-    if (id == R.id.menu_more) {
-      if (!binding.toolbox.isExpanded()) {
-        binding.toolbox.expand();
-      } else {
-        binding.toolbox.collapse();
-      }
-      return true;
+        ViewCompat.setOnApplyWindowInsetsListener(binding.fab, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            MarginLayoutParams mlp = (MarginLayoutParams) v.getLayoutParams();
+            mlp.bottomMargin = insets.bottom;
+            v.setLayoutParams(mlp);
+            return WindowInsetsCompat.CONSUMED;
+        });
+
+        binding.term.setOnClickListener(it -> 
+            startActivity(new Intent(getApplicationContext(), TermuxActivityCompat.class))
+        );
     }
-    if (id == R.id.menu_undo) {
-      binding.editor.undo();
-      return true;
-    }
-    if (id == R.id.menu_redo) {
-      binding.editor.redo();
-      return true;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    this.binding = null;
-  }
+        if (id == R.id.menu_more) {
+            if (!binding.toolbox.isExpanded()) {
+                binding.toolbox.expand();
+            } else {
+                binding.toolbox.collapse();
+            }
+            return true;
+        }
+        if (id == R.id.menu_undo) {
+            binding.editor.undo();
+            return true;
+        }
+        if (id == R.id.menu_redo) {
+            binding.editor.redo();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
 }
--- app/src/main/java/com/sparkleside/SettingsActivity.java
+++ app/src/main/java/com/sparkleside/SettingsActivity.java
@@ -1,56 +1,40 @@
 package com.sparkleside;
 
-import com.google.android.material.appbar.AppBarLayout;
-import android.content.Intent;
-import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener;
-import androidx.activity.EdgeToEdge;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import com.sparkleside.databinding.ActivitySettingsBinding;
 import android.animation.ArgbEvaluator;
+import android.content.Intent;
+import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.sparkleside.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends AppCompatActivity {
+
     private ActivitySettingsBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        
-	    setSupportActionBar(binding.toolbar);
+
+        setSupportActionBar(binding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        binding.toolbar.setNavigationOnClickListener(v -> {
-			onBackPressed();
-			}
-		);
-      binding.about.setOnClickListener(v->{
-          Intent intent = new Intent(SettingsActivity.this,AboutActivity.class);
-            startActivity(intent);
-      }
-        );
-       binding.main.setOnClickListener(v->{
-          Intent intent = new Intent(SettingsActivity.this,AppearanceActivity.class);
-            startActivity(intent);
-      }
-        );
-        
-      binding.appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                int totalScrollRange = appBarLayout.getTotalScrollRange();
-                float scrollPercentage = Math.abs(verticalOffset) / (float) totalScrollRange;
-                ArgbEvaluator argbEvaluator = new ArgbEvaluator();                
-                int toolbarColor = (int) argbEvaluator.evaluate(scrollPercentage, getColor(R.color.md_theme_surface), getColor(R.color.md_theme_surfaceVariant));                
-                binding.appbar.setBackgroundColor(toolbarColor);
-            }
-        });  
-    }
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-}
 
+        binding.toolbar.setNavigationOnClickListener(v -> super.onBackPressed());
+
+        binding.about.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, AboutActivity.class);
+            startActivity(intent);
+        });
+
+        binding.main.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, AppearanceActivity.class);
+            startActivity(intent);
+        });
+    }
 }
