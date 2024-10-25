e8ea9365a91a7f3693aad0dc9065505d990a441d
trindadedev13 <devsuay@gmail.com>
2024-10-25 00:08:06 -0300

    fix: BaseActivity must be open

--- app/src/main/java/com/sparkleside/ui/base/BaseActivity.kt
+++ app/src/main/java/com/sparkleside/ui/base/BaseActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class BaseActivity : AppCompatActivity(){
+open class BaseActivity : AppCompatActivity(){
 
         override fun onCreate(saved: Bundle?) {
                 super.onCreate(saved)
