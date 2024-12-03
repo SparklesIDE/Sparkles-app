7e1e0ae6a1eddb9d578a851dd7781b7f80675fa8
trindadedev13 <trindadedev13@gmail.com>
2024-12-03 06:44:27 +0000

    chore: Format Kotlin Code
    format code with ktfmt in github actions

--- ui-utils/src/main/java/dev/trindadedev/ui_utils/components/preferences/PreferencePopup.kt
+++ ui-utils/src/main/java/dev/trindadedev/ui_utils/components/preferences/PreferencePopup.kt
@@ -15,7 +15,7 @@ package dev.trindadedev.ui_utils.preferences
  *
  *  You should have received a copy of the GNU General Public License
  *   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
- */ 
+ */
 
 import android.content.Context
 import android.util.AttributeSet
@@ -24,50 +24,44 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.PopupMenu
 import android.widget.TextView
-
 import dev.trindadedev.ui_utils.R
 
-class PreferencePopup @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+class PreferencePopup
+@JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+  LinearLayout(context, attrs, defStyleAttr) {
 
-    public val preferenceTitle: TextView
-    public val preferenceDescription: TextView
-    public val preference: View
-    val popupMenu: PopupMenu = PopupMenu(context, this)
+  public val preferenceTitle: TextView
+  public val preferenceDescription: TextView
+  public val preference: View
+  val popupMenu: PopupMenu = PopupMenu(context, this)
 
-    init {
-        LayoutInflater.from(context).inflate(R.layout.layout_preference, this, true)
+  init {
+    LayoutInflater.from(context).inflate(R.layout.layout_preference, this, true)
 
-        preferenceTitle = findViewById(R.id.preference_title)
-        preferenceDescription = findViewById(R.id.preference_description)
-        preference = findViewById(R.id.preference)
+    preferenceTitle = findViewById(R.id.preference_title)
+    preferenceDescription = findViewById(R.id.preference_description)
+    preference = findViewById(R.id.preference)
 
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.PreferencePopup,
-            0, 0
-        ).apply {
-            try {
-                val title = getString(R.styleable.PreferencePopup_preferencePopupTitle) ?: ""
-                val description = getString(R.styleable.PreferencePopup_preferencePopupDescription) ?: ""
-                preferenceTitle.text = title
-                preferenceDescription.text = description
-            } finally {
-                recycle()
-            }
-        }
-
-        preference.setOnClickListener { popupMenu.show() }
+    context.theme.obtainStyledAttributes(attrs, R.styleable.PreferencePopup, 0, 0).apply {
+      try {
+        val title = getString(R.styleable.PreferencePopup_preferencePopupTitle) ?: ""
+        val description = getString(R.styleable.PreferencePopup_preferencePopupDescription) ?: ""
+        preferenceTitle.text = title
+        preferenceDescription.text = description
+      } finally {
+        recycle()
+      }
     }
 
-    fun addPopupMenuItem(itemTitle: String) {
-        popupMenu.menu.add(itemTitle)
-    }
+    preference.setOnClickListener { popupMenu.show() }
+  }
 
-    fun setMenuListener(listener: PopupMenu.OnMenuItemClickListener) {
-        popupMenu.setOnMenuItemClickListener(listener)
-    }
+  fun addPopupMenuItem(itemTitle: String) {
+    popupMenu.menu.add(itemTitle)
+  }
+
+  fun setMenuListener(listener: PopupMenu.OnMenuItemClickListener) {
+    popupMenu.setOnMenuItemClickListener(listener)
+  }
 }
