5e57a940cca3e078f5d4b3647ea496d581a95c26
SyntaxSpin <sketcherwaree@gmail.com>
2024-12-22 01:42:00 +0100

    Returning JavaCompiler

---
+++ java-compiler/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  id("com.android.library")
+  id("kotlin-android")
+}
+
+android {
+  namespace = "org.robok.engine.feature.compiler.java"
+  compileSdk = 34
+    
+  defaultConfig {
+    minSdk = 21
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  compilerOptions {
+    jvmTarget = JvmTarget.JVM_17
+  }
+}
+
+dependencies {
+  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+  implementation("com.android.tools:r8:8.3.37")
+}
